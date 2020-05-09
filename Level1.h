4848fb23143eacad553b3463ffed5f38f446ce3b
Erica Chou <57302666+Faitsu@users.noreply.github.com>
2020-05-09 15:22:51 -0400

    Delete Level1.h

--- Level1.h
+++
@@ -1,9 +0,0 @@
-#include "Scene.h"
-class Level1 : public Scene { 
-	public: 
-	int enemyCount;
-
-	void Initialize() override;   
-	void Update(float deltaTime) override;  
-	void Render(ShaderProgram *program) override; 
-};
